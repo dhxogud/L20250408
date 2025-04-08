c34b384367e22c0d82aec7f4e96fda5e43f05756
dhxogud <hyong0117@naver.com>
2025-04-08 15:03:37 +0900

    1차 수정

--- Main.cpp
+++ Main.cpp
@@ -2,37 +2,48 @@
 
 using namespace std;
 
-int main()
+int* DynamicArray = new int[10];
+
+void Initialize()
 {
-	int* DynamicArray = new int[10];
-	
 	for (int i = 0; i < 10; ++i)
 	{
 		DynamicArray[i] = (i + 1) * 3;
 	}
+}
 
-	int* NewArray = new int[20];
-	for (int i = 0; i < 20; ++i)
+void ResizeDynamicArray()
+{
+	for (int i = 1; i < 11; ++i)
 	{
-		if (i < 10)
+		memset(DynamicArray, i, sizeof(int));
+		*((DynamicArray + 9) + i) = i * 4;
+	}
+
+}
+
+void PrintResult()
+{
+	if (DynamicArray != nullptr)
+	{
+		for (int i = 0; i < 20; ++i)
 		{
-			*(NewArray + i) = *(DynamicArray + i);
+			cout << DynamicArray[i] << endl;
 		}
-		else
-		{
-			*(NewArray + i) = (i - 9) * 4;
-		}
+	}
+	else
+	{
+		cout << "Dangling Pointer!" << endl;
 	}
 	
-	delete[] DynamicArray;
+}
 
-	DynamicArray = NewArray;
 
-	for (int i = 0; i < 20; ++i)
-	{
-		cout << DynamicArray[i] << endl;
-	}
-
+int main()
+{
+	Initialize();
+	ResizeDynamicArray();
+	PrintResult();
 
 	return 0;
 }
